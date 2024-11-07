3d309078b8832e34c088a9de4bfb8923f9c6e7fe
strugglinglee <strugglinglee@163.com>
2024-11-07 16:40:13 +0800

    chore: init

---
+++ src/views/main/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <header class="h-[60px] bg-[#b3c0d1] flex items-center justify-center">头部</header>
+    <div class="flex flex-1">
+      <div class="w-[200px] bg-[#d3dce6] order-1">左侧边栏</div>
+      <div class="flex-1 bg-[#e9eef3] order-2">中间内容区</div>
+      <div class="w-[200px] bg-[#d3dce6] order-3">右侧边栏</div>
+    </div>
+    <footer class="h-[60px] bg-[#b3c0d1] flex items-center justify-center">底部</footer>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
